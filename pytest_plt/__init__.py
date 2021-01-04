6fa9a6c598e31a7a7ad2cb33459cadb612491c7f
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2021-01-04 11:54:31 -0500

    Update copyright dates to 2021

--- pytest_plt/__init__.py
+++ pytest_plt/__init__.py
@@ -7,5 +7,5 @@ pytest fixtures for quickly creating Matplotlib plots in your tests.
 
 from .version import version as __version__
 
-__copyright__ = "2018-2019 pytest_plt contributors"
+__copyright__ = "2018-2021 pytest_plt contributors"
 __license__ = "MIT license"
